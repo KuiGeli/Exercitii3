e4bee218c8a55a069770e8e35a78ec00b2359df4
KuiGeli <gelikui11@gmail.com>
2019-09-10 22:22:59 +0300

    Exc1 solved

--- src/Exc1.java
+++ src/Exc1.java
@@ -5,22 +5,24 @@ public class Exc1 {
         int number1 = 15;
         int number2 = 20;
 
-        int flag = 0;
+        int numberOfPrimes = 0;
 
         int i;
 
         for (i = number1; i <= number2; i++) {
-
+            int flag = 0;
         for (int k = 2; k<i; k++){
             if (i%k == 0){
                 flag =+ 1;
             }
         }
             if (flag == 0){
-                System.out.println(i);
-                System.out.println(flag);
+                System.out.println(i + " is prime");
+                numberOfPrimes++;
+
             }
         }
+        System.out.println("There were " + numberOfPrimes + " prime numbers");
 
 
     }
