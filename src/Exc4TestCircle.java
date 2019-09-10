6e289e43839cbdd2df5b7ac918694e5a56369eba
KuiGeli <gelikui11@gmail.com>
2019-09-10 15:11:09 +0300

    a

--- src/Exc4TestCircle.java
+++ src/Exc4TestCircle.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
-
 public class Exc4TestCircle {
+
+    public static void main(String[] args) {
+        Exc4Circle circleTest = new Exc4Circle(2.4);
+        Exc4Circle circleTest2 = new Exc4Circle("Blue");
+
+        System.out.println(circleTest.getColor() + " " + circleTest.getRadius());
+        System.out.println(circleTest2.getColor() + " " + circleTest2.getRadius());
+
+    }
+
 }
