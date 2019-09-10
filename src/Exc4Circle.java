0eddda1d57fae0d98b8a7ded4bba807af53f71a1
KuiGeli <gelikui11@gmail.com>
2019-09-10 15:05:36 +0300

    gitIgnore added

---
+++ src/Exc4Circle.java
@@ -0,0 +1,21 @@
+public class Exc4Circle {
+
+    private double radius = 1.0;
+    private String color = "red";
+
+
+    public Exc4Circle (double newRadius){
+        this.radius = newRadius;
+    }
+    public Exc4Circle(String newColor){
+        this.color = newColor;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
